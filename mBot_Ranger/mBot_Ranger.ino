22bf82696c200fffd28f53572bd4985193b7dd48
Matilda <55895602+akma18rd@users.noreply.github.com>
2023-04-04 17:20:14 +0200

    got the mower to move forwards and backwards and to turn in a random direction while backing when ultrasound sensor is closer than 5 cm to an object

--- mBot_Ranger/mBot_Ranger.ino
+++ mBot_Ranger/mBot_Ranger.ino
@@ -1,11 +1,92 @@
+#include <MeAuriga.h>
 #include <Arduino.h>
+
+
+MeUltrasonicSensor ultraSensor(PORT_7);
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+
+void avoidObject();
+void moveForward();
+void moveBackwards();
+void turnRandomLeftAndBacking();
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  //Set PWM 8KHz
+  TCCR1A = _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+
+  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
+}
+
+void loop() { //read distance in cm
+  // put your main code here, to run repeatedly:
+  Serial.print("Distance : ");
+  Serial.print(ultraSensor.distanceCm() );
+  Serial.println(" cm");
+
+  moveForward();
+  // Serial.print("Speed 1:");
+  // Serial.print(Encoder_1.getCurrentSpeed());
+  // Serial.print(" ,Speed 2:");
+  // Serial.println(Encoder_2.getCurrentSpeed());
+
+
+  if (ultraSensor.distanceCm() <= 5)
+  {
+    long int start = millis();
+    long int end = start + 4000;
+    long int doneBacking = start + 2000;
+    
+    while(end >= millis()){
+      if(doneBacking >= millis()){
+        moveBackwards();
+      }
+      else
+      {
+        turnRandomLeftAndBacking();
+      }
+      
+    }
+    Serial.println("I left the loop");
+    
+  }
+  Serial.println("I left the if statement and :" + String(ultraSensor.distanceCm()));
+
+  Encoder_1.loop();
+  Encoder_2.loop();
+  delay(100); /* the minimal measure interval is 100 milliseconds */
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void moveForward(){
+  Encoder_1.setMotorPwm(-100);
+  Encoder_2.setMotorPwm(100);
+}
 
+void moveBackwards(){
+  Encoder_1.setMotorPwm(100);
+  Encoder_2.setMotorPwm(-100);
+} 
+
+void avoidObject(){
+  
+  while (ultraSensor.distanceCm() <= 15)
+  {
+    moveBackwards();
+  }
+
+  turnRandomLeftAndBacking();
+  
+}
+
+void turnRandomLeftAndBacking(){ //45 degrees turn left backward
+  int randomNumber = random(100, 200);
+
+  Encoder_1.setMotorPwm(100);
+  Encoder_2.setMotorPwm(-randomNumber);
 }
