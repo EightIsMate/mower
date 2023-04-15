f73ec037a5b88bdd6e413b5049935297345fc39f
Matilda <55895602+akma18rd@users.noreply.github.com>
2023-04-15 13:40:52 +0200

    changed some of the constant names to make sense in hte functionality

--- mBot_Ranger/mBot_Ranger.ino
+++ mBot_Ranger/mBot_Ranger.ino
@@ -2,17 +2,17 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define AURIGARINGLEDNUM  12
-#define RINGALLLEDS        0
+#define AURIGARINGLEDNUM 12
+#define RINGALLLEDS 0
 
 #ifdef MeAuriga_H
 
 // on-board LED ring, at PORT0 (onboard), with 12 LEDs
-MeRGBLed led_ring( 0, 12 );
+MeRGBLed led_ring(0, 12);
 
 #endif
 
-//linefollower sensor 
+// linefollower sensor
 MeLineFollower lineFinder(PORT_9);
 
 MeGyro gyro(0, 0x69);
@@ -20,7 +20,7 @@ MeUltrasonicSensor ultraSensor(PORT_7);
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 
-bool isOnEdge = false;
+bool anySensorOnLine = false;
 bool foundLine = false;
 int sensorState = 0;
 
@@ -29,22 +29,23 @@ void moveForward();
 void moveBackwards();
 void turnRandomLeftAndBacking();
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   gyro.begin();
-  
-  #ifdef MeAuriga_H
-    // 12 LED Ring controller is on Auriga D44/PWM
-    led_ring.setpin( 44 );
-  #endif
-  
+
+#ifdef MeAuriga_H
+  // 12 LED Ring controller is on Auriga D44/PWM
+  led_ring.setpin(44);
+#endif
+
   while (!Serial)
   {
-    ; //Wait for serial port to connect to raspberry pi
+    ; // Wait for serial port to connect to raspberry pi
   }
-  
-  //Set PWM 8KHz
+
+  // Set PWM 8KHz
   TCCR1A = _BV(WGM10);
   TCCR1B = _BV(CS11) | _BV(WGM12);
 
@@ -53,48 +54,49 @@ void setup() {
 }
 
 // put your main code here, to run repeatedly:
-void loop() { 
-  
-  //communication with raspberry pi
+void loop()
+{
+
+  // communication with raspberry pi
   char raspCom;
 
   if (Serial.available() > 0)
-  {  
+  {
     raspCom = Serial.read();
     switch (raspCom)
     {
     case '0':
       // all LEDs off
-      led_ring.setColor( RINGALLLEDS, 0, 0, 0 );
+      led_ring.setColor(RINGALLLEDS, 0, 0, 0);
       led_ring.show();
       delay(500);
       Serial.write("A", 1);
       break;
-    
+
     case '1':
-      led_ring.setColor( RINGALLLEDS, 0, 50, 0 );
+      led_ring.setColor(RINGALLLEDS, 0, 50, 0);
       led_ring.show();
       delay(500);
       Serial.write("A", 1);
     default:
-    Serial.write("E", 1);
+      Serial.write("E", 1);
       break;
     }
   }
 
-  //read distance in cm
+  // read distance in cm
   /*
   Serial.print("Distance : ");
   Serial.print(ultraSensor.distanceCm() );
   Serial.println(" cm");*/
 
-  //read and print Z coordinate from gyro
+  // read and print Z coordinate from gyro
   gyro.update();
   /*
   Serial.read();
   Serial.print(" Z:");
   Serial.println(gyro.getAngleZ() );*/
-  
+
   int x = 0;
   int y = 0;
   /*
@@ -103,93 +105,109 @@ void loop() {
     x = gyro.getAngleX();
     y = gyro.getAngleY();
     Serial.print(" X:");
-    Serial.println(x ); 
+    Serial.println(x );
     Serial.print(" Y:");
-    Serial.println(y );  
+    Serial.println(y );
   }
-  
+
 
     x = gyro.getAngleX();
     y = gyro.getAngleY();
     Serial.print(" X:");
-    Serial.println(x ); 
+    Serial.println(x );
     Serial.print(" Y:");
-    Serial.println(y );  
+    Serial.println(y );
 */
 
-/*
-  if (ultraSensor.distanceCm() <= 5)
-  {
-    long int start = millis();
-    long int end = start + 4000;
-    long int doneBacking = start + 2000;
-    
-    while(end >= millis()){
-      if(doneBacking >= millis()){
-        moveBackwards();
+
+
+  /*
+    if (ultraSensor.distanceCm() <= 5)
+    {
+      long int start = millis();
+      long int end = start + 4000; //4 seconds later
+      long int doneBacking = start + 2000;
+
+      while(end >= millis()){
+        if(doneBacking >= millis()){
+          moveBackwards();
+        }
+        else
+        {
+          turnRandomLeftAndBacking();
+        }
+
       }
-      else
-      { 
-        turnRandomLeftAndBacking();
-      }
-      
+      //Serial.println("I left the loop");
+
     }
-    //Serial.println("I left the loop");
-    
-  }
-  //Serial.println("I left the if statement and :" + String(ultraSensor.distanceCm()));
-  */
+    //Serial.println("I left the if statement and :" + String(ultraSensor.distanceCm()));
+    */
 
   Encoder_1.loop();
   Encoder_2.loop();
   delay(100); /* the minimal measure interval is 100 milliseconds */
 
-  //read the line follower sensors
-  //long int now = millis(); 
+  // read the line follower sensors
+  // long int now = millis();
   sensorState = lineFinder.readSensors();
 
- // Serial.println(sensorState);
+  Serial.println(sensorState);
 
-  // only one of the sensors on black line
-  if ((sensorState == S1_IN_S2_OUT )|| (sensorState == S1_OUT_S2_IN)){
-    isOnEdge = true;
+  // if any single sensor is on black line
+  if ((sensorState == S1_IN_S2_OUT) || (sensorState == S1_OUT_S2_IN))
+  {
+    anySensorOnLine = true;
   }
 
-  //stay inside confined area
-  if((sensorState == S1_IN_S2_IN) || (isOnEdge == true)){
-    turnRandomLeftAndBacking();
-    isOnEdge = false;
-  }else{
+
+  // stay inside confined area
+  if ((sensorState == S1_IN_S2_IN) || (anySensorOnLine == true))
+  {
+    if (foundLine == false)
+    {
+      moveBackwards();
+      anySensorOnLine = false;
+      foundLine = true;
+    }
+  }
+  else
+  {
     moveForward();
+    foundLine = false;
   }
 }
 
-void moveForward(){
+void moveForward()
+{
   Encoder_1.setMotorPwm(-100);
   Encoder_2.setMotorPwm(100);
 }
 
-void moveBackwards(){
+void moveBackwards()
+{
+   long int start = millis();
+   long int end = start + 2000; //4 seconds later
+   
   Encoder_1.setMotorPwm(100);
   Encoder_2.setMotorPwm(-100);
-} 
+}
 
-void avoidObject(){
-  
+void avoidObject()
+{
+
   while (ultraSensor.distanceCm() <= 15)
   {
     moveBackwards();
   }
 
   turnRandomLeftAndBacking();
-  
 }
 
-void turnRandomLeftAndBacking(){ //45 degrees turn left backward
+void turnRandomLeftAndBacking()
+{ // 45 degrees turn left backward
   int randomNumber = random(100, 200);
 
   Encoder_1.setMotorPwm(100);
   Encoder_2.setMotorPwm(-randomNumber);
-
 }
-
