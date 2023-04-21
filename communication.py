bd18e2b18f59f451789d4bfae80d0c06f01e8c58
bagen1 <elinbage22@gmail.com>
2023-04-21 10:07:27 +0200

    Prints out if a client discnnects

--- communication.py
+++ communication.py
@@ -2,12 +2,15 @@ import asyncio
 import websockets
 
 async def handle_client(websocket, path):
-    print(f'Connection from: {websocket.remote_address}')
-
-    message = await websocket.recv()
-    print(f'Received: {message}')
-
-    await websocket.send('Hello from Raspberry Pi!')
+    try:
+        print(f'Connection from: {websocket.remote_address}')
+        async for message in websocket:
+            print(f"Received message: {message}")
+            await websocket.send("Hello from Raspberry Pi 4!")
+    except websockets.ConnectionClosedError as e:
+        print(f"Connection closed: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 start_server = websockets.serve(handle_client, '172.20.10.9', 12345) #Elins hotspot
 
